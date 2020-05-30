3d96e7bb75ca5fcbfdcc2a225cdefd283c0dafac
marcusfooo <43901768+marcusfooo@users.noreply.github.com>
2020-05-30 14:16:09 +0800

    Create webscraper.py
    Scrape Amazon page

---
+++ webscraper.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "https://www.amazon.sg/s?k=" # Base url link
+search_item = input("Enter search input: ") # Search input
+
+# Search from pages 1-20
+for page in range(1, 20, 1):
+    url = base_url+search_item+"&page="+str(page) # Add page number to link
+    req = requests.get(url) # Fetch url
+    content = req.content # Read url
+    soup = BeautifulSoup(content, "html.parser") # Parse html content
+    all = soup.find_all("div", {"class": "s-result-item"}) # Find all search items in page
+
+    # For every item in the page, do this
+    for item in all:
+
+        # Create an empty dictionary
+        d= {}
+
+        #Populate dictionary values with search results
+        try:
+            d["name"] = item.find("span", {"class": "a-text-normal"}).text
+            d["price"] = item.find("span", {"class": "a-offscreen"}).text
+            d["rating"] = item.find("span", {"class": "a-icon-alt"}).text
+            print(d["name"],'\n',d["price"],'\n',d["rating"],'\n')
+        except:
+            pass
+
+            
+        
